b1e45cd0776573bf8eccba45b58ed7c3e2ab5436
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-02-03 20:34:31 -0500

    formatting. no functionality change

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken);
-    console.log('refreshToken', refreshToken);
-    console.log('profile:', profile);
-}
-)
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken);
+        console.log('refreshToken', refreshToken);
+        console.log('profile:', profile);
+        }
+    )
 );
 
 app.get('/auth/google',

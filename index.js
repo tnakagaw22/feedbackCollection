fb8727be73743f7a179366ce803f5ff3972eb2f7
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-02-13 22:18:55 -0500

    installed coolie-session and define max age and cookie key

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -8,6 +10,15 @@ const authRoutes = require('./routes/authRoutes');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days, 24 hr, 60 mins, 60 secs, 1000 millisecs
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 

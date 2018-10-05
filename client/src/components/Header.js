a8370f5d9b684fbe3e8340ccf953be3511baf339
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-10-05 18:09:22 -0400

    added header component with materialize css

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="left brand-logo">Emaily</a>
+                    <ul className="right">
+                        <li><a href="sass.html">Login with Google</a></li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header;

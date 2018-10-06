49738597bc761f28de844578278d8d27de59fe51
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-10-06 19:26:40 -0400

    added .env variable for stripe key so that we can have different key for dev and prod

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -19,11 +20,16 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="#" className="left brand-logo">Emaily</a>
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
+                        Emaily
+                    </Link>
                     <ul className="right">
-                    {
-                        this.renderContent()
-                    }
+                        {
+                            this.renderContent()
+                        }
                     </ul>
                 </div>
             </nav>
